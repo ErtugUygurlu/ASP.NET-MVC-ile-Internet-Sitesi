d663452d09d5ab6c43d2a5356a7abcc94982c63c
ErtugUygurlu <ertu_ygrl@hotmail.com>
2023-02-10 14:32:29 +0300

    Add project files.

---
+++ datafirst/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using datafirst.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace datafirst.Controllers
+{
+    public class LoginController : Controller
+    {
+        ErtuDBEntities db = new ErtuDBEntities();
+
+        public ActionResult Index()
+        {
+            var model = db.Author.ToList();
+            return View();
+        }
+    }
+}
---
+++ datafirst/Controllers/ProjectController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace datafirst.Controllers
+{
+    public class ProjectController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Harail | Projeler";
+            return View();
+        }
+    }
+}
